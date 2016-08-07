46ba0cccd411d2c8e1d6cb9d344b8e8b63bc9b58
Jihoon Sung <sungjihoon@gmail.com>
2016-08-08 01:25:12 +0900

    Spring Security Setting

--- src/main/java/net/jihoon/P2P/web/IndexController.java
+++ src/main/java/net/jihoon/P2P/web/IndexController.java
@@ -2,6 +2,7 @@ package net.jihoon.P2P.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Jihoon Sung on 2016-08-04.
@@ -10,6 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/authentication", method = RequestMethod.GET)
+    public String authentication() {
         return "index";
     }
 }
